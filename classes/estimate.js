b4847326577ce3e41e51e4bef1b33487014e52d2
unknown <wolfleader@wolfleader-PC.(none)>
2011-11-12 17:57:10 -0500

    my work for Nov 12

---
+++ classes/estimate.js
@@ -0,0 +1,47 @@
+    //Creating an empty estimate
+    function estimate() {
+    var estimateID = 0; //some Mongo DB method for assigning ID should go here
+    var estimateSubtotal = 0;
+    var estimateFinalTotal = 0;
+    var estimateStatus = 1; //Setting the estimate status to active by default
+    var estimateLineItem  = new Array();
+    var estimateCreatedDate  = new Date(); //Set estimateCreatedDate to current date;
+     
+    }
+     
+    this.getEstimateID = function (){
+    return this.estimateID;
+    }
+     
+    this.getEstimateSubtotal = function(){
+    return this.estimateSubtotal;
+    }
+     
+     
+    this.getEstimateFinaltotal = function(){
+    return this.estimateFinalTotal;
+    }
+     
+    this.getEstimateStatus = function(){
+    return this.estimateStatus;
+    }
+     
+     
+    this.getEstimateLineItem = function(){
+    return this.estimateLineItem;
+    }
+     
+    this.getEstimateCreatedDate = function(){
+    return this.estimateCreatedDate
+    }
+     
+     
+    this.addEstimateLineItem = function(){
+    EstimateLineItem.setEstimateLineItem(estimateID);
+    }
+     
+    this.setEstimateStatus = function(status){
+            if(status >= 1 && status <= 3){
+               estimateLineItem = status
+              }
+    }
