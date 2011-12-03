510affcbf4ff104c0c314838f7c06978f22e312e
Dennis Villasenor <falschirmjager@msn.com>
2011-12-03 15:19:07 -0500

    another commit for dec 3

--- classes/estimate.js
+++ classes/estimate.js
@@ -1,47 +1,96 @@
-    //Creating an empty estimate class
-    function estimate() {
-    var estimateID = 0; //some Mongo DB method for assigning ID should go here
-    var estimateSubtotal = 0;
-    var estimateFinalTotal = 0;
-    var estimateStatus = 1; //Setting the estimate status to active by default
-    var estimateLineItem  = new Array();
-    var estimateCreatedDate  = new Date(); //Set estimateCreatedDate to current date;
-     
-    }
-     
-    this.getEstimateID = function (){
-    return this.estimateID;
-    }
-     
-    this.getEstimateSubtotal = function(){
-    return this.estimateSubtotal;
-    }
-     
-     
-    this.getEstimateFinaltotal = function(){
-    return this.estimateFinalTotal;
-    }
-     
-    this.getEstimateStatus = function(){
-    return this.estimateStatus;
-    }
-     
-     
-    this.getEstimateLineItem = function(){
-    return this.estimateLineItem;
-    }
-     
-    this.getEstimateCreatedDate = function(){
-    return this.estimateCreatedDate
-    }
-     
-     
-    this.addEstimateLineItem = function(){
-    EstimateLineItem.setEstimateLineItem(estimateID);
-    }
-     
-    this.setEstimateStatus = function(status){
-            if(status >= 1 && status <= 3){
-               estimateLineItem = status
-              }
-    }
+Estimate = function (options) {
+options = options || {};
+
+var ID = options.ID || 0; //some Mongo DB method for assigning ID should go here
+var Subtotal options.Subtotal || 0;
+var FinalTotal options.FinalTotal || 0;
+var Status options.Status || 1; //Setting the estimate status to active by default
+var LineItemSet  options.LineItem || new Array();
+var CreatedDate  options.CreatedDate || new Date(); //Set estimateCreatedDate to current date;
+
+Object.defineProperty(this, "ID", {
+value: ID,
+writable: false,
+get: function () { return ID; },
+
+});
+
+Object.defineProperty(this, "Subtotal", {
+get: function () { return Subtotal; },
+enumerable: true,
+});
+
+Object.defineProperty(this, "FinalTotal", {
+get: function () { return FinalTotal; },
+enumerable: true,
+});
+
+Object.defineProperty(this, "Status", {
+get: function () { return Status; },
+set: function (newStatus) { Status = newStatus; },
+enumerable: true,
+});
+
+Object.defineProperty(this, "LineItemSet", {
+        get: function () { return LineItemSet; },
+        set: function (newLineItemSet) { LineItemSet = newLineItemSet; },
+enumerable: true,
+});
+
+Object.defineProperty(this, "CreatedDate", {
+get: function () { return CreatedDate; },
+set: function (newCreatedDate) { Status = newCreatedDate; },
+});
+}
+
+
+Estimate.prototype = {
+
+addJob: function (job) {
+    addEstimateLineItem: function (EstimateLineItem) {
+        if (EstimateLineItem instanceof EstimateLineItem) {
+            this.LineItemSet.push(EstimateLineItem);
+        }
+},
+
+    addEstimateLineItem: function (options) {
+        var estimateLineItem = new EstimateLineItem(options);
+        this.LineItemSet.push(estimate);
+    },
+
+    removeEstimateLineItem: function (estimateLineItem) {
+   		var size = LineItemSet.length;
+		
+  		for (i=0;i<=size;i++){
+				if(LineItemSet[i] == estimateLineItem)
+					{
+						LineItemSet.splice (i);
+					}
+			}
+    },	
+	
+    calculateSubTotal: function (LineItemSet) {
+        
+		var estimateLineItem;
+		var size = LineItemSet.length;
+		var i=0;
+		
+		for (i=0;i<=size;i++){
+			estimateLineItem = LineItemSet[i];
+			Subtotal = Subtotal + estimateLineItem.total;
+			}
+		 return this.Subtotal;
+    },
+
+
+    calculateFinalTotal: function (Subtotal) {
+        
+			FinalTotal = Subtotal * 1.15; //subtotal + taxes 
+			}
+		 return this.FinalTotal;
+    },
+
+}
+
+exports.Estimate = Estimate;
+
