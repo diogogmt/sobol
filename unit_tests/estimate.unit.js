6182a7d1e959b8fcba6804150e66c0e406de03d9
Dennis Villasenor <falschirmjager@msn.com>
2011-12-03 14:54:21 -0500

    Update for Dec 3

--- unit_tests/estimate.unit.js
+++ unit_tests/estimate.unit.js
@@ -7,31 +7,46 @@ var Estimate = models.Estimate;
 var EstimateLineItem = models.EstimateLineItem;
 
 var estimateOptions = {
-ID 201
-Status 1 //Setting the estimate status to active
+ID : 201
+Status  = 1 //Setting the estimate status to active
 LineItemSet : [new estimateLineItem(estimateLineItem1)];
 CreatedDate =  new Date(); //Set estimateCreatedDate to current date; 
 
 },
 
 var estimateLineItem1= {
-
+id : 100,
+name: 'Test1',
+description: 'Test Item 1',
+quantity 5,
+cost 5,
 
 }
 
 var estimateLineItem2= {
-
+id : 101,
+name: 'Test2',
+description: 'Test Item 2',
+quantity 5,
+cost 5,
 
 }
 
 var estimateLineItem3= {
-
+id : 102,
+name: 'Test3',
+description: 'Test Item 3',
+quantity 5,
+cost 5,
 
 }
 
 var estimateLineItem4= {
-
-
+id : 103,
+name: 'Test4',
+description: 'Test Item 4',
+quantity 5,
+cost 5,
 }
 
 
@@ -44,9 +59,9 @@ vows.describe('Customer').addBatch()
       : function (topic) {
 				topic.addEstimateLineItem(estimateLineItem2);
 		'check that Subtotal is correct'
-		assert.equal(topic.Subtotal, 0);
+		assert.equal(topic.Subtotal, 50);
 		'check that Final total is correct'
-		assert.equal(topic.FinalTotal, 0);
+		assert.equal(topic.FinalTotal, 57.5);
 		},
    
 		}
@@ -59,9 +74,9 @@ vows.describe('Customer').addBatch()
 				topic.addEstimateLineItem(estimateLineItem3);
 				topic.addEstimateLineItem(estimateLineItem4);
 		'check that Subtotal is correct'
-		assert.equal(topic.Subtotal, 0);
+		assert.equal(topic.Subtotal, 100);
 		'check that Final total is correct'
-		assert.equal(topic.FinalTotal, 0);
+		assert.equal(topic.FinalTotal, 115);
 		},
    
 		}
@@ -75,9 +90,9 @@ vows.describe('Customer').addBatch()
       : function (topic) {
 				topic.removeEstimateLineItem(estimateLineItem2);
 		'check that Subtotal is correct'
-		assert.equal(topic.Subtotal, 0);
+		assert.equal(topic.Subtotal, 75);
 		'check that Final total is correct'
-		assert.equal(topic.FinalTotal, 0);
+		assert.equal(topic.FinalTotal, 86.25);
 		},
    
 		}
@@ -91,9 +106,9 @@ vows.describe('Customer').addBatch()
 				topic.removeEstimateLineItem(estimateLineItem3);
 				topic.removeEstimateLineItem(estimateLineItem4);
 		'check that Subtotal is correct'
-		assert.equal(topic.Subtotal, 0);
+		assert.equal(topic.Subtotal, 25);
 		'check that Final total is correct'
-		assert.equal(topic.FinalTotal, 0);
+		assert.equal(topic.FinalTotal, 28.75);
 		},
    
 		}
