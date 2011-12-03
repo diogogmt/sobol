34d2f848ffaac5c2b092dd7dbc53bfbcf194f261
Dennis Villasenor <falschirmjager@msn.com>
2011-12-03 14:41:22 -0500

    estimate unit test dec 3

--- unit_tests/estimate.unit.js
+++ unit_tests/estimate.unit.js
@@ -1,41 +1,104 @@
 var vows = require('vows'),
     assert = require('assert');
 
-var estimate = require('../classes/estimate.js');
+var estimate = require('../classes/model.js');
 
-var Strawberry   = estimate.Strawberry,
-    Banana       = estimate.Banana,
-    PeeledBanana = estimate.PeeledBanana;
+var Estimate = models.Estimate;
+var EstimateLineItem = models.EstimateLineItem;
 
-vows.describe('The Good Things').addBatch({
-    'A strawberry': {
-        topic: new(Strawberry),
+var estimateOptions = {
+ID 201
+Status 1 //Setting the estimate status to active
+LineItemSet : [new estimateLineItem(estimateLineItem1)];
+CreatedDate =  new Date(); //Set estimateCreatedDate to current date; 
 
-        'is red': function (strawberry) {
-            assert.equal (strawberry.color, '#ff0000');
-        },
-        'and tasty': function (strawberry) {
-            assert.isTrue (strawberry.isTasty());
-        }
-    },
-    'A banana': {
-        topic: new(Banana),
+},
 
-        'when peeled *synchronously*': {
-            topic: function (banana) {
-                return banana.peelSync();
-            },
-            'returns a `PeeledBanana`': function (result) {
-                assert.instanceOf (result, PeeledBanana);
-            }
-        },
-        'when peeled *asynchronously*': {
-            topic: function (banana) {
-                banana.peel(this.callback);
-            },
-            'results in a `PeeledBanana`': function (err, result) {
-                assert.instanceOf (result, PeeledBanana);
-            }
-        }
-    }
-}).export(module); // Export the Suite
+var estimateLineItem1= {
+
+
+}
+
+var estimateLineItem2= {
+
+
+}
+
+var estimateLineItem3= {
+
+
+}
+
+var estimateLineItem4= {
+
+
+}
+
+
+vows.describe('Customer').addBatch() 
+{
+
+'Calculate Subtotal and Final Total after adding one line item': {
+    : {
+      topic: new Estimate(estimateOptions),
+      : function (topic) {
+				topic.addEstimateLineItem(estimateLineItem2);
+		'check that Subtotal is correct'
+		assert.equal(topic.Subtotal, 0);
+		'check that Final total is correct'
+		assert.equal(topic.FinalTotal, 0);
+		},
+   
+		}
+   
+	}
+'Calculate Subtotal and Final Total after adding two line items': {
+    : {
+      topic: new Estimate(estimateOptions),
+      : function (topic) {
+				topic.addEstimateLineItem(estimateLineItem3);
+				topic.addEstimateLineItem(estimateLineItem4);
+		'check that Subtotal is correct'
+		assert.equal(topic.Subtotal, 0);
+		'check that Final total is correct'
+		assert.equal(topic.FinalTotal, 0);
+		},
+   
+		}
+   
+	}
+
+
+'Calculate Subtotal and Final Total after removing one line items': {
+    : {
+      topic: new Estimate(estimateOptions),
+      : function (topic) {
+				topic.removeEstimateLineItem(estimateLineItem2);
+		'check that Subtotal is correct'
+		assert.equal(topic.Subtotal, 0);
+		'check that Final total is correct'
+		assert.equal(topic.FinalTotal, 0);
+		},
+   
+		}
+   
+	}
+
+'Calculate Subtotal and Final Total after removing two line items': {
+    : {
+      topic: new Estimate(estimateOptions),
+      : function (topic) {
+				topic.removeEstimateLineItem(estimateLineItem3);
+				topic.removeEstimateLineItem(estimateLineItem4);
+		'check that Subtotal is correct'
+		assert.equal(topic.Subtotal, 0);
+		'check that Final total is correct'
+		assert.equal(topic.FinalTotal, 0);
+		},
+   
+		}
+   
+	}
+
+}
+  
