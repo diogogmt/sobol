1d54fc781505cbc846155f40dfa574c8f9efb983
Dennis <dennis@ubuntu.(none)>
2012-02-06 22:46:51 -0500

    still figuring out undefined error

--- routes/job.js
+++ routes/job.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose')
   , models = require('./../models')
+  , config = require('./../config')
   , domain = 'http://localhost:11342/'
   , Schema = mongoose.Schema;
 
@@ -14,14 +15,13 @@ exports.all = function (req, res) {
 };
 
 
-
 exports.add = function(req, res) {
   console.log("add job route");
-  var job = new Job(req.body.job);
+//  var job = new Job(req.body.job);
   console.log("job: %o", job);
   function jobSaveFailed() {
     console.log("failed creating job");
-//need to confirm if we need to redirect to customers or job
+// need to confirm if we need to redirect to customers or job
   }
 
   job.save(function(err) {
@@ -30,10 +30,31 @@ exports.add = function(req, res) {
       return jobSaveFailed();
     } 
     console.log("creating job");
-    req.flash('info', 'Job has been added');
+  //  req.flash('info', 'Job has been added');
   });
 };
 
 exports.findAll = function (req, res) {
   console.log("all customers route");
 };
+
+
+
+
+/*
+exports.all = function (req, res) {
+  console.log("all jobs route");
+  return 1;
+};
+
+exports.add = function(req, res) {
+  console.log("add job route");
+  return 1;
+};
+
+exports.findAll = function (req, res) {
+  console.log("all customers route");
+  return 1;
+};
+
+*/
