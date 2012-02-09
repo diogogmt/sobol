48d1e70bb566d4f936d1c55b9501be8785b3fd22
Dennis <dennis@ubuntu.(none)>
2012-02-08 19:22:31 -0500

    semi working test version.

--- routes/job.js
+++ routes/job.js
@@ -12,12 +12,18 @@ models.defineModels(function() {
 exports.all = function (req, res) {
   console.log("all jobs route");
   console.log("req.currentUser: %o", req.currentUser);
+  res.render('customer/customers', 
+    {
+      layout: 'includes/layout',
+      title: 'Jobs'
+    });
 };
 
-
+ 
 exports.add = function(req, res) {
   console.log("add job route");
-//  var job = new Job(req.body.job);
+
+  var job = new Job(req.body.job);
   console.log("job: %o", job);
   function jobSaveFailed() {
     console.log("failed creating job");
@@ -30,10 +36,38 @@ exports.add = function(req, res) {
       return jobSaveFailed();
     } 
     console.log("creating job");
-    req.flash('info', 'Job has been added');
+    res.redirect('/customers');
+//    req.flash('info', 'Job has been added');
   });
+  
 };
 
 exports.findAll = function (req, res) {
   console.log("all customers route");
+
+ Job.find({}, function (err, jobs) {
+    console.log("Find Job ");
+    if(jobs){
+      console.log("get all jobs success");
+      //console.log(customers);
+
+      var dataSet = new Array();
+      for(i = 0; i < jobs.length; i++){
+        dataSet.push([
+            jobs[i].customid,
+            jobs[i].name,
+            jobs[i].description
+      
+        ]);
+      }
+
+      res.json(aaData);
+    }
+    else {
+      console.log("get all jobs Not success");
+    }
+  });
+
+
+
 };
