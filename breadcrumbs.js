79ac54c6105abac562805d0bc9b7747291374ee3
Diogo Golovanevsky Monteiro <diogo.gmt@gmail.com>
2012-03-20 22:49:41 -0400

    Dynamically creating breadcrumbs.

---
+++ breadcrumbs.js
@@ -0,0 +1,39 @@
+var Customer = require('./models').Customer
+  , Job = require('./models').Job;
+
+exports.createBreadcrumb = function (opt, callback) {
+  console.log("createBreadcrumbs");
+  console.log("opt: ", opt);
+  if (opt.hasOwnProperty("customerID")) {
+    console.log("has custID prop");
+    exports.getCustomer(opt.customerID, function (customer) {
+      console.log("customer breadcrumb created");
+       return callback({
+         "cust": customer
+       });
+    });
+  }
+
+  return null;
+};
+
+exports.getCustomer = function (custID, callback) {
+  console.log("getCustomer");
+  console.log("custID: ", custID);
+
+  Customer.findOne( { "_id":  custID }, ["firstName", "lastName"], function (err, cust) {
+
+    console.log("err: ", err);
+    console.log("cust: ", cust);
+
+    if (cust) {
+      return callback({
+        "id": cust._id,
+        "name": cust.firstName + " " + cust.lastName,
+      });
+    }
+    return callback(null);
+  });
+
+};
+
