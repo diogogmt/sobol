71e4b180e9b020f1973c4f439312c9a322fb1271
Diogo Golovanevsky Monteiro <diogo.gmt@gmail.com>
2012-03-24 22:14:27 -0400

    Updating jobManagement.

--- public/js/jobManagement.js
+++ public/js/jobManagement.js
@@ -8,89 +8,107 @@ var JobManagement = function (opt) {
 
   this.estimateDataTable = null;
 
-  console.log("this.customerId: " + this.customerId);
-  console.log("this.jobId: " + this.jobId);
-  // Templates
-  this.addEstimateOverlayTmpl = $("#addEstimateOverlayTmpl");
-  this.estimateDataTableOverlayTmpl = $("#estimateDataTableOverlayTmpl");
-  this.editJobmTmpl = $("#editJobTmpl");
-
-  // Elements
-  this.addEstimateOverlay = $("#addEstimateOverlay");
-  this.estimateTable = $("#estimateTable");
-  this.editJobForm = $("#editJobForm");
-  this.addtEstimateForm = $("#addEstimateForm");
-  // Buttons
-  this.addEstimateBtn = $("#addEstimateBtn");
-  this.saveEstimateBtn = $("#saveEstimateBtn");
-  this.editJobBtn = $("#editJobBtn");
-  console.log("$('#editJobBtn')" + $("#editJobBtn"));
-
   this.addEstimateOverlayObj;
 
   this.initOverlays();
-  this.loadTemplates();
   this.loadDatePicker();
   this.loadEstimateDataTable();
   this.createHandlers();
 };
 
 
-
-JobManagement.prototype.loadTemplates = function() {
-  console.log("loadTemplates");
-  console.log(this);
-  console.log(this.editJobmTmpl);
-  this.addEstimateOverlayTmpl.tmpl({}).appendTo(this.addEstimateOverlay);
-  this.estimateDataTableOverlayTmpl.tmpl({}).appendTo(this.estimateTable);
-  this.editJobmTmpl.tmpl({}).appendTo(this.editJobForm);
-};
-
-JobManagement.prototype.validateAddEstimateFields = function() {
-  console.log("validateAddEstimateFields");
-  // do validation
-  return true;
-};
-
-JobManagement.prototype.getAddEstimateFields = function() {
-  console.log("validateAddEstimateFields");
+// Initialize overlay object
+JobManagement.prototype.initOverlays = function() {
+  console.log("initOverlays");
   var that = this;
-  console.log("name");
-  console.log($("#estimateNameTxt").val());
-  return {
-    'estimate': {
-      "name": $("#estimateNameTxt").val(),
-    }
-  };
+
+  $(that.addEstimateOverlay).overlay({
+    top: 260,
+    mask: {
+      color: '#fff',
+      loadSpeed: 200
+    },
+    closeOnClick: false,
+  });
+  this.addEstimateOverlayObj = $("#addEstimateOverlay").overlay({});
+
+  $(that.addEstimateOverlay).overlay({
+    top: 260,
+    mask: {
+      color: '#fff',
+      loadSpeed: 200
+    },
+    closeOnClick: false,
+  });
+  this.addEstimateOverlayObj = $("#addEstimateOverlay").overlay({});
+
 };
 
+// Create Event Handlers
 JobManagement.prototype.createHandlers = function() {
   console.log("createHandlers");
-  this.createAddEstimateHandler();
-  this.createCancelEstimateHandler();
-  this.createSaveEstimateHandler();
-  this.createEditJobHandler();
+  this.createEstimateOverlayHandler();
+  this.cancelEstimateHandler();
+  this.saveEstimateHandler();
+  this.editJobHandler();
 };
 
 
-JobManagement.prototype.createAddEstimateHandler = function() {
-  console.log("createAddEstimateHandler");
+// Edit job  button handler
+JobManagement.prototype.editJobHandler = function() {
+  console.log("editJobHandler");
   var that = this;
-  $(this.addEstimateBtn).live("click", function (e) {
-    console.log("addEstimateBtn click");
+  console.log("$(editoBtn)" + $("#editJobBtn"));
+  $("#editJobBtn").click(function (e) {
+    console.log("EditJobBtn click");
+    if (that.validateEditJobFields()) {
+      console.log("form is valid");
+      console.log("fields: " + that.getEditJobFields());
+      $.ajax({
+        url: "/job/" + that.jobId + "/edit",
+        type: "POST",
+        data: that.getEditJobFields(),
+        success: function (data) {
+          console.log("success");
+          console.log(data);
+        },
+        error: function () {
+          console.log("error");
+        },
+        complete: function () {
+          console.log("complete");
+        }
+      }); // END ajax
+    }
+    return false;
+  }); // END saveEstimateBtn click
+};
+
+// Estimate overlay button handler
+JobManagement.prototype.createEstimateOverlayHandler = function() {
+  console.log("createEstimateOverlayHandler");
+  var that = this;
+  $("#createEstimateOverlayBtn").click(function (e) {
+    console.log("createEstimateBtn click");
     that.addEstimateOverlayObj.load();
   });
 };
 
-JobManagement.prototype.createCancelEstimateHandler = function() {
-  console.log("createCancelEstimateHandler");
-
+// Cancel estimate button handler
+JobManagement.prototype.cancelEstimateHandler = function() {
+  console.log("cancelEstimateHandler");
+  var that = this;
+  $("#cancelEstimateBtn").click(function (e) {
+    console.log("cancel estimate button");
+    that.addEstimateOverlayObj.close();
+  });
 };
 
-JobManagement.prototype.createSaveEstimateHandler = function() {
-  console.log("createSaveEstimateHandler");
+// Save estimate button handler
+JobManagement.prototype.saveEstimateHandler = function() {
+  console.log("saveEstimateHandler");
   var that = this;
-  $(this.saveEstimateBtn).live("click", function (e) {
+  $("#saveEstimateBtn").click(function (e) {
     console.log("saveEstimateBtn click");
     if (that.validateAddEstimateFields()) {
       console.log("form is valid");
@@ -102,6 +120,7 @@ JobManagement.prototype.createSaveEstimateHandler = function() {
         success: function (data) {
           console.log("success");
           console.log(data);
+          $("#estimateName").val("");
           that.loadEstimateDataTable();
           that.addEstimateOverlayObj.close();
         },
@@ -117,46 +136,48 @@ JobManagement.prototype.createSaveEstimateHandler = function() {
   }); // END saveEstimateBtn click
 };
 
-JobManagement.prototype.loadEstimateDataTable = function() {
-  console.log("loadEstimateDataTable");
+// Validate Add Estimate Fields
+JobManagement.prototype.validateAddEstimateFields = function() {
+  console.log("validateAddEstimateFields");
+  // do validation
+  return true;
 };
 
-JobManagement.prototype.initOverlays = function() {
-  console.log("initOverlays");
+// Get Estimate Fields
+JobManagement.prototype.getAddEstimateFields = function() {
+  console.log("validateAddEstimateFields");
   var that = this;
-
-  $(that.addEstimateOverlay).overlay({
-    // custom top position
-    top: 260,
-    // some mask tweaks suitable for facebox-looking dialogs
-    mask: {
-      // you might also consider a "transparent" color for the mask
-      color: '#fff',
-      // load mask a little faster
-      loadSpeed: 200
-    },
-    // disable this for modal dialog-type of overlays
-    closeOnClick: false,
-  });
-  this.addEstimateOverlayObj = $(that.addEstimateOverlay).overlay({});
-
-  $(that.addEstimateOverlay).overlay({
-    // custom top position
-    top: 260,
-    // some mask tweaks suitable for facebox-looking dialogs
-    mask: {
-      // you might also consider a "transparent" color for the mask
-      color: '#fff',
-      // load mask a little faster
-      loadSpeed: 200
-    },
-    // disable this for modal dialog-type of overlays
-    closeOnClick: false,
-  });
-  this.addEstimateOverlayObj = $(that.addEstimateOverlay).overlay({});
-
+  console.log("name");
+  return {
+    'estimate': {
+      "name": $("#estimateName").val(),
+    }
+  };
 };
 
+// Validate Edit Jobs Fields
+JobManagement.prototype.validateEditJobFields = function() {
+  console.log("validateUpdateJobFields");
+  // do validation
+  return true;
+};
+
+// Get Edit Job Fields
+JobManagement.prototype.getEditJobFields = function() {
+  console.log("getUpdateJobFields");
+  var that = this;
+  console.log("name");
+  return {
+    'job': {
+      "name": $("#jobName").val(),
+      "description": $("#jobDesc").val(),
+      "status": $("#jobStatus").val(),
+      "scheduledDates": $('#datepicker').multiDatesPicker('getDates'),
+    }
+  };
+};
+
+// Load estimates datatable
 JobManagement.prototype.loadEstimateDataTable = function() {
   console.log("loadEstimateDataTabel");
   var that = this;
@@ -178,61 +199,7 @@ JobManagement.prototype.loadEstimateDataTable = function() {
     });
 };
 
-
-JobManagement.prototype.createEditJobHandler = function() {
-  console.log("createEditJobHandler");
-  var that = this;
-  console.log("this.editJobBtn: " + this.editJobBtn);
-  console.log("$(editoBtn)" + $("#editJobBtn"));
-  $("#editJobBtn").click(function (e) {
-    console.log("EditJobBtn click");
-    if (that.validateEditJobFields()) {
-      console.log("form is valid");
-      console.log("fields: " + that.getEditJobFields());
-      $.ajax({
-        url: "/job/" + that.jobId + "/edit",
-        type: "POST",
-        data: that.getEditJobFields(),
-        success: function (data) {
-          console.log("success");
-          console.log(data);
-          // that.loadEstimateDataTable();
-          // that.addEstimateOverlayObj.close();
-        },
-        error: function () {
-          console.log("error");
-        },
-        complete: function () {
-          console.log("complete");
-        }
-      }); // END ajax
-    }
-    return false;
-  }); // END saveEstimateBtn click
-};
-
-JobManagement.prototype.validateEditJobFields = function() {
-  console.log("validateUpdateJobFields");
-  // do validation
-  return true;
-};
-
-JobManagement.prototype.getEditJobFields = function() {
-  console.log("getUpdateJobFields");
-  var that = this;
-  console.log("name");
-  console.log($("#estimateNameTxt").val());
-  return {
-    'job': {
-      "name": $("#jobName").val(),
-      "description": $("#jobDesc").val(),
-      "status": $("#jobStatus").val(),
-      "scheduledDates": $('#datepicker').multiDatesPicker('getDates'),
-    }
-  };
-};
-
-
+// Load date picker
 JobManagement.prototype.loadDatePicker = function() {
   console.log("loadDatePicker");
   console.log("this.dates: ", this.dates);
